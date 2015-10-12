2e6b9ce2eb57adcf8c6d3a8140a4b87873b9b46a
juaguzman <juancarlos_891111@hotmail.com>
2015-10-12 11:30:43 -0500

    casa

--- app/src/main/java/example/juancarlos/com/fecuncarnaval/Main2Activity.java
+++ app/src/main/java/example/juancarlos/com/fecuncarnaval/Main2Activity.java
@@ -1,13 +1,96 @@
 package example.juancarlos.com.fecuncarnaval;
 
+
+
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+
+import com.facebook.FacebookSdk;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 public class Main2Activity extends AppCompatActivity {
 
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+
+
+        String imgurl = getUrlFacebookUserAvatar("http://graph.facebook.com/100000351746269/picture?type=large");
+        new DownloadImageTask((ImageView) findViewById(R.id.profileImageView))
+                .execute(imgurl);
+
+
     }
+
+    public static String getUrlFacebookUserAvatar(String name_or_idUser )
+    {
+        String address = "http://graph.facebook.com/"+name_or_idUser+"/picture";
+        URL url;
+        String newLocation = null;
+        try {
+            url = new URL(address);
+            HttpURLConnection.setFollowRedirects(false); //Do _not_ follow redirects!
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            newLocation = connection.getHeaderField("Location");
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return newLocation;
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", " " + e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result)
+        {
+            bmImage.setImageBitmap(result);
+        }
+    }
+
+
+
 }
