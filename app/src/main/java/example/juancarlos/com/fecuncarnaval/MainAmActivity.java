b3499cd4734ab6e3526c1eb407eac5fd23160013
juaguzman <juancarlos_891111@hotmail.com>
2015-10-12 22:39:08 -0500

    arreglo

--- app/src/main/java/example/juancarlos/com/fecuncarnaval/MainAmActivity.java
+++ app/src/main/java/example/juancarlos/com/fecuncarnaval/MainAmActivity.java
@@ -1,13 +1,35 @@
 package example.juancarlos.com.fecuncarnaval;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MainAmActivity extends AppCompatActivity {
+public class MainAmActivity extends AppCompatActivity implements View.OnClickListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activityd_amigos);
+
+        ImageButton imgBtn28 = (ImageButton) findViewById(R.id.imgBtna28);
+        imgBtn28.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v)
+    {
+     switch (v.getId())
+     {
+         case (R.id.imgBtna28):
+             Intent j = new Intent(this, MainActivityAmigos.class);
+             j.putExtra("fecha", "2015-12-30");
+             startActivity(j);
+             break;
+
+     }
     }
 }
