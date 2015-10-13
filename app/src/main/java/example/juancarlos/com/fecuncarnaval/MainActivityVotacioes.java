5d4bb602970edc1f436e86077ec3b0c8e3b74146
fernandatorres <fernandatorres1909@hotmail.com>
2015-10-12 21:23:28 -0500

    v

--- app/src/main/java/example/juancarlos/com/fecuncarnaval/MainActivityVotacioes.java
+++ app/src/main/java/example/juancarlos/com/fecuncarnaval/MainActivityVotacioes.java
@@ -5,7 +5,12 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 /**
  * Created by usuario on 12/10/2015.
@@ -16,15 +21,29 @@ public class MainActivityVotacioes extends Activity
 
     int [] fotos =
             {
-                    R.drawable.da6,
-                    R.drawable.da6,
-                    R.drawable.da6,
+                    R.drawable.cr1,
+                    R.drawable.cr2,
+                    R.drawable.cr3,
+                    R.drawable.cr4,
+                    R.drawable.cr5,
+                    R.drawable.cr6,
             };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         listado=(ListView) findViewById(R.id.listViewVotaciones);
+        Adapter adapter = new ListViewAdapter(this, fotos);
+
+        listado.setAdapter((ListAdapter) adapter);
+
+        listado.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "position" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
