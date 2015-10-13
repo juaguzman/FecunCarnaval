998b3126b22ab6470f597214c9172bd2cba846bb
juaguzman <juancarlos_891111@hotmail.com>
2015-10-13 08:05:32 -0500

    tratando hacer listView con imagenes

--- app/src/main/java/example/juancarlos/com/fecuncarnaval/MainActivityFtoVt.java
+++ app/src/main/java/example/juancarlos/com/fecuncarnaval/MainActivityFtoVt.java
@@ -2,28 +2,39 @@ package example.juancarlos.com.fecuncarnaval;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.RatingBar;
 
-public class MainActivityFtoVt extends AppCompatActivity
-{
+public class MainActivityFtoVt extends AppCompatActivity implements View.OnClickListener {
     private RatingBar ratingBar;
+    private ImageButton imgBtnVot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity_fto_vt);
+        imgBtnVot = (ImageButton) findViewById(R.id.imgBtnVot);
+        imgBtnVot.setOnClickListener(this);
         addListenerOnRatingBar();
 
     }
     public void addListenerOnRatingBar()
     {
 
-        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        ratingBar = (RatingBar) findViewById(R.id.rtgBar);
 
          ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
 
             }
         });
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        String.valueOf(ratingBar.getRating());
+
     }
 }
