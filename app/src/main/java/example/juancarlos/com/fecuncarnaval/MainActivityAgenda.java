d244a3a6ea438b5a137183ca69fee25d0c988dfc
juaguzman <juancarlos_891111@hotmail.com>
2015-10-15 19:40:11 -0500

    prueba

---
+++ app/src/main/java/example/juancarlos/com/fecuncarnaval/MainActivityAgenda.java
@@ -0,0 +1,16 @@
+package example.juancarlos.com.fecuncarnaval;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by usuario on 13/10/2015.
+ */
+public class MainActivityAgenda extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.agenda);
+    }
+}
