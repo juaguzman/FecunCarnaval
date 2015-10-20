c56065750c91204fe66154312d86fe3c67064358
fernandatorres <fernandatorres1909@hotmail.com>
2015-10-20 15:37:21 -0500

    cambios

--- app/src/main/java/example/juancarlos/com/fecuncarnaval/MainActivityAmigos.java
+++ app/src/main/java/example/juancarlos/com/fecuncarnaval/MainActivityAmigos.java
@@ -3,17 +3,26 @@ package example.juancarlos.com.fecuncarnaval;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,7 +39,7 @@ public class MainActivityAmigos extends AppCompatActivity implements View.OnClic
     private String id,evento,descripcion,lugar,horaIni,horaFin,categoria,idUsu,fecha;
     RequestQueue requestQueue;
 
-    String insertUrl = "http://festum1.comule.com/getProgramad.php?fecha="+fecha;
+    String insertUrl = "http://festum1.comule.com/insertMiProg.php";
 
     private ImageButton imgbtnNa;
     private ImageButton imgbtnAa;
@@ -54,29 +63,56 @@ public class MainActivityAmigos extends AppCompatActivity implements View.OnClic
         imgbtnNa.setOnClickListener(this);
         imgbtnCa = (ImageButton) findViewById(R.id.imgbtnCa);
         imgbtnCa.setOnClickListener(this);
+        obtDatos();
 
+        registerForContextMenu(listado);
+
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.add("Ver descripcion");
+        menu.add("Agregar a mi agenda");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+
+        super.onContextItemSelected(item);
+
+        if (item.getTitle()=="Ver descripcion")
+        {
+            pos = info.position;
+            //prueba(pos);
+
+        }
+        else if (item.getTitle()=="Agregar a mi agenda")
+        {
+            //AgregarEvento(pos);
+            Toast.makeText(this, "Evento agregado", Toast.LENGTH_LONG).show();
+        }
+
+        return true;
     }
 
     public void obtDatos()
     {
         Bundle bundle=getIntent().getExtras();
-        fecha = bundle.getString("fecha");
+        fechass = bundle.getString("fecha");
         AsyncHttpClient client = new AsyncHttpClient();
-
+        String url = "http://festum1.comule.com/getProgramad.php?fecha="+fechass;
 
         RequestParams parametros = new RequestParams();
-        parametros.put("fecha",fecha);
+        parametros.put("fecha",fechass.toString());
 
-
-        client.post(url, parametros, new AsyncHttpResponseHandler()
-        {
+        client.post(url, parametros, new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
                 if (statusCode == 200) {
                     //llamar a la funcion
-                    CargarLista(obbDatosJSON(new String(responseBody)));
-
-
+                    obbDatosJSON(new String(responseBody));
                 }
             }
 
@@ -88,29 +124,34 @@ public class MainActivityAmigos extends AppCompatActivity implements View.OnClic
 
     }
 
-    public void CargarLista(ArrayList<String> datos)
+    public void obbDatosJSON(String response)
     {
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,datos);
-        listado.setAdapter(adapter);
-    }
+        obtenerIdusu();
 
-    public ArrayList<String> obbDatosJSON(String response)
-    {
-        ArrayList<String> listado =new ArrayList<String>();
         try
         {
             JSONArray jsonArray = new JSONArray(response);
-            String texto;
+
+            listprog = new ArrayList<Programa>();
+            Programa act;
 
             for (int i=0; i<jsonArray.length();i++)
             {
-                texto=jsonArray.getJSONObject(i).getString("evento") + "\n " +
-                        jsonArray.getJSONObject(i).getString("horaInicio") + "-"+
-                        jsonArray.getJSONObject(i).getString("horaFin") + " ";
-
-                listado.add(texto);
+                id = jsonArray.getJSONObject(i).getString("id");
+                fecha = jsonArray.getJSONObject(i).getString("fecha");
+                evento = jsonArray.getJSONObject(i).getString("evento");
+                descripcion = jsonArray.getJSONObject(i).getString("descripcion");
+                lugar = jsonArray.getJSONObject(i).getString("lugar");
+                horaIni = jsonArray.getJSONObject(i).getString("horaInicio");
+                horaFin = jsonArray.getJSONObject(i).getString("horaFin");
+                categoria = jsonArray.getJSONObject(i).getString("categoria");
+                act = new Programa(id,fecha,evento,descripcion,lugar,horaIni,horaFin,categoria,idUsu);
+                listprog.add(act);
 
             }
+            adapter = new ItemListAdapterProg(this, listprog);
+            listado.setAdapter(adapter);
+            adapter.notifyDataSetChanged();
 
         }
         catch (Exception e)
@@ -118,8 +159,35 @@ public class MainActivityAmigos extends AppCompatActivity implements View.OnClic
             e.printStackTrace();
 
         }
-        return listado;
+
     }
+
+
+    public void obtenerIdusu()
+    {
+        GraphRequest request = GraphRequest.newMeRequest(
+                AccessToken.getCurrentAccessToken(),
+                new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        // Application code
+                        try {
+
+                            idUsu = String.valueOf(object.get("id"));
+
+
+                            // String email = (String) object.get("email");
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "id,name,email,gender, birthday");
+        request.setParameters(parameters);
+        request.executeAsync();    }
 
     @Override
     public void onClick(View v)
