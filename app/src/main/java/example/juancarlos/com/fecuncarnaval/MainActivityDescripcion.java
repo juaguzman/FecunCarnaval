ba7dc55350043b5bee4dd291fd10d531129f7318
juaguzman <juancarlos_891111@hotmail.com>
2015-10-20 16:33:42 -0500

    cosa

--- app/src/main/java/example/juancarlos/com/fecuncarnaval/MainActivityDescripcion.java
+++ app/src/main/java/example/juancarlos/com/fecuncarnaval/MainActivityDescripcion.java
@@ -52,7 +52,7 @@ public class MainActivityDescripcion extends Activity
         Bundle bundle=getIntent().getExtras();
         id = (bundle.getString("id"));
         AsyncHttpClient client = new AsyncHttpClient();
-        String url = "http://festum1.comule.com/getProgramad.php?id="+id;
+        String url = "http://festum1.comule.com/getProgramaId.php?id="+id;
 
         RequestParams params = new RequestParams();
         params.put("id", id.toString());
